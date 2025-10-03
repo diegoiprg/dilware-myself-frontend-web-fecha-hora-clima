80243c8433b0dd6c1f839cf8c257617785dea020
diegoiprg <diego.iparraguirre@gmail.com>
2025-10-03 17:31:28 -0500

    v1.6.0

--- src/components/app/LocationDisplay.tsx
+++ src/components/app/LocationDisplay.tsx
@@ -1,7 +1,10 @@
+import { MapPin } from 'lucide-react';
+
 export const LocationDisplay = ({ displayName }: { displayName?: string }) => (
   <div className="w-full landscape:w-1/2 flex items-center justify-center landscape:justify-start">
-    <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-muted-foreground text-center landscape:text-left whitespace-nowrap">
-      {displayName || '...'}
+    <div className="flex items-center gap-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-muted-foreground text-center landscape:text-left whitespace-nowrap">
+      <MapPin className="size-6 sm:size-7 md:size-8 lg:size-9" />
+      <span>{displayName || '...'}</span>
     </div>
   </div>
 );
